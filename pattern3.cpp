f69e0e306b5cb11bb4a726bb6e3cbdf0bcedd623
Deepak Chaudhary <94126562+wartuall@users.noreply.github.com>
2022-12-06 23:57:59 +0530

    Update pattern3.cpp

--- pattern3.cpp
+++ pattern3.cpp
@@ -27,77 +27,77 @@ Output:
 @*******@
 *********
 */   
-//     int n;
-//     cin >> n;
-//     int t = 0;
-//     for (int i = 1; i <= n - n / 2; ++i)
-//     {
-//         for (int j = 1; j <= (n - n / 2) - i; j++)
-//         {
-//             cout << " ";
-//         }
-//         for (int j = 1; j <= i; j++)
-//         {
-//             cout << "*";
-//         }
+    int n;
+    cin >> n;
+    int t = 0;
+    for (int i = 1; i <= n - n / 2; ++i)
+    {
+        for (int j = 1; j <= (n - n / 2) - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
 
-//         for (int j = 2; j <= i; ++j)
-//         {
-//             cout << "*";
-//         }
-//         cout << endl;
-//     }
-//     for (int i = 1; i <= n; i++)
-//     {
-//         if (i <= n - n / 2)
-//         {
-//             for (int j = 1; j <= 1; j++)
-//             {
-//                 cout << "@";
-//             }
-//             for (int j = 1; j <= n - 2; j++)
-//             {
-//                 cout << " ";
-//             }
-//             for (int j = 1; j <= 1; j++)
-//             {
-//                 cout << "@";
-//             }
-//             cout << endl;
-//         }
-//         else
-//         {
-//             ++t;
-//             for (int j = 1; j <= 1; j++)
-//             {
-//                 cout << "@";
-//             }
-//             for (int j = 1; j <= n / 2 - t; j++)
-//             {
-//                 cout << " ";
-//             }
-//             for (int j = 1; j <= t; ++j)
-//             {
-//                 cout << "*";
-//             }
-//             for (int j = 2; j <= t; ++j)
-//             {
-//                 cout << "*";
-//             }
-//             for (int j = 1; j <= n / 2 - t; j++)
-//             {
-//                 cout << " ";
-//             }
-//             for (int j = 1; j <= 1; j++)
-//             {
-//                 cout << "@";
-//             }
-//             cout << endl;
-//         }
-//     }
-//     for (int j = 1; j <= n; j++)
-//     {
-//         cout << "*";
-//     }
+        for (int j = 2; j <= i; ++j)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (i <= n - n / 2)
+        {
+            for (int j = 1; j <= 1; j++)
+            {
+                cout << "@";
+            }
+            for (int j = 1; j <= n - 2; j++)
+            {
+                cout << " ";
+            }
+            for (int j = 1; j <= 1; j++)
+            {
+                cout << "@";
+            }
+            cout << endl;
+        }
+        else
+        {
+            ++t;
+            for (int j = 1; j <= 1; j++)
+            {
+                cout << "@";
+            }
+            for (int j = 1; j <= n / 2 - t; j++)
+            {
+                cout << " ";
+            }
+            for (int j = 1; j <= t; ++j)
+            {
+                cout << "*";
+            }
+            for (int j = 2; j <= t; ++j)
+            {
+                cout << "*";
+            }
+            for (int j = 1; j <= n / 2 - t; j++)
+            {
+                cout << " ";
+            }
+            for (int j = 1; j <= 1; j++)
+            {
+                cout << "@";
+            }
+            cout << endl;
+        }
+    }
+    for (int j = 1; j <= n; j++)
+    {
+        cout << "*";
+    }
     return 0;
 }
